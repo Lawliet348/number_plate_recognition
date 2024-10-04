6f8075765710a72945bb4fb15bffb4bdc45721ab
Anurav Aryan <121193186+Lawliet348@users.noreply.github.com>
2024-10-04 13:11:01 +0530

    Added cert.pem and key.pem for https connections.

--- app.py
+++ app.py
@@ -65,6 +65,6 @@ def results():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
-    # app.run(debug=True, host="0.0.0.0", port=8000, ssl_context=("cert.pem", "key.pem"))
+    # app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=8000, ssl_context=("cert.pem", "key.pem"))
     # app.run(debug=True, host="127.0.0.1", port=8000, ssl_context=("cert.pem", "key.pem"))
